ac51ae19e7c059b6c4ecfa1ac9010d34bcefec78
yixuan <yixuan.coder@gmail.com>
2021-12-05 17:38:25 +0800

    g1

--- demo.py
+++ demo.py
@@ -1,33 +1,3 @@
-def add_in_gitlearn():
-    pass
-
-def add(a, b):
-    return a + b
-
-def f3():
-    pass
-
 def foo():
     pass
 
-
-def spam():
-    pass
-
-
-def m2():
-    pass
-    
-def test():
-    pass
-
-f1 = 'gitlearnbranch'
-f2 = 'gitlearnbranch'
-f3 = 'gitlearnbranch'
-f4 = 'gitlearnbranch'
-
-def test_merge_1():
-    pass
-
-def add_by_github():
-    pass
